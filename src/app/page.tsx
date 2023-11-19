50e644115ee258507496c5d583a563b9b36319bc
RasmusElmersson <rasmus.elmersson@martinservera.se>
2023-11-19 16:17:26 +0100

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { ThemeSwitcher } from "@/components/theme-switcher";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen">
+      <div className="">
+        <ThemeSwitcher />
+      </div>
+    </main>
+  );
+}
